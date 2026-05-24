0ece6b25a1e86734757fef070d6a6be95fb6de0d
Psp32 <prempatro317@gmail.com>
2026-05-24 20:33:11 +0530

    websocket client logic added

--- Loopin-Client/src/App.tsx
+++ Loopin-Client/src/App.tsx
@@ -1,27 +1,32 @@
-import Button from "./components/Button";
-import ChatIcon from "./icons/ChatIcon";
+import { useEffect, useRef } from "react";
+import ChatRoom from "./pages/ChatRoom";
+import LandingPage from "./pages/LandingPage";
+
+// Later implement useSocket or React Context
 
 export default function App() {
+    const socketRef = useRef<WebSocket | null>(null)
+
+    useEffect(()=>{
+      const socket = new WebSocket("ws://localhost:8080")
+      socketRef.current = socket
+      socket.onopen=()=>{
+          console.log("workss")
+      }
+
+      socket.onmessage=(event)=>{
+          console.log(event.data)
+      }
+
+      socket.onerror=()=>{
+          console.log("erorr occured")
+      }
+    },[])
 
   return (
     <>
-    <div className="flex flex-col min-h-screen bg-black justify-center items-center pb-50 text-white">
-      <div className="flex flex-col justify-center border border-gray-600 p-5 rounded-lg m-5 pt-10">
-        <div className="flex text-3xl gap-2 left-0 items-center pl-10">
-          <ChatIcon/> Real Time Chat
-        </div>
-      <div className="text-lg text-slate-400 pl-10">
-        Temporary room that expires after both users exit
-      </div>
-      <div className="flex items-center justify-center pt-8">
-        <Button text="Create New Room" size="lg"/>
-      </div>
-      <div className="flex m-10">
-        <input type="text" placeholder="Enter Room Code" className="text-white border border-gray-300 rounded-md text-center pr-40 mr-5"/>
-        <Button text="Join Room" size="sm"/>
-      </div>
-      </div>
-    </div>
+      <LandingPage socket={socketRef}/>
+      <ChatRoom socket={socketRef}/>
     </>
   )
 }
